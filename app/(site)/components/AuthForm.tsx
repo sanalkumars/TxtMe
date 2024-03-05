262cac63db362ee9f36647a5b3b1b036c9cf4db0
sanalkumars <sanalskumar126@gmail.com>
2024-03-05 21:38:17 +0530

    stared the auth part

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -1,6 +1,7 @@
  'use client';
 
 import { useCallback, useState } from "react";
+import {  FieldValues,SubmitHandler, useForm } from "react-hook-form";
 
 type Variant = 'LOGIN' | 'REGISTER';
 
@@ -15,6 +16,37 @@ const AuthForm = () => {
             setVariant('LOGIN')
         }
     },[variant]);
+
+    const {
+      register,
+      handleSubmit,
+      formState: {
+        errors
+      }
+    } = useForm<FieldValues>({
+      defaultValues:{
+        name: '',
+        email :'',
+        password: ''
+      }
+    });
+
+    const onSubmit : SubmitHandler<FieldValues> = (data) =>{
+      setIsLoading(true);
+      if(variant === 'REGISTER'){
+        // axios register
+      }
+      if(variant === 'LOGIN'){
+        // auth signin 
+      }
+    }
+
+    const socialAction = (action: string) =>{
+      setIsLoading(true);
+      // nextt auth social signin
+    }
+
+
   return (
     <div>AuthForm</div>
   )
